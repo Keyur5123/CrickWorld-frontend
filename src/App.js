33e918700fe63e9cb2c0f0a49ee81db79db80d2d
Keyur Pipaliya <keyurpipaliya4597@gmail.com>
2022-07-08 14:53:12 +0530

    google SEO keywords & Site description added in index.html file

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import ContactUs from './Screens/ContactUs';
 import News from './Screens/News';
 import Info from './Screens/Info';
 import Privacy_Policy from "./Screens/Privacy_Policy";
+import { Helmet } from "react-helmet";
 
 function App(props) {
 
@@ -16,6 +17,15 @@ function App(props) {
     <div>
       <BrowserRouter>
         <Header />
+
+        <Helmet>
+          <meta
+            name="Live Cricket Score"
+            content='odi top player score analysis,live match,cricket,ipl 2021,ipl live score,cricket score,live cricket score,ipl score,ipl 2020,today ipl match,live cricket,t20 world cup,ipl live score,cricket icc,virat kohli,cricbuzz live sc,ipl matchipl 2021 live,t20 world cup,cricbuzz live,icc world cup,yesterday ipl,england cricket,india cricket,india live score,live cricket tv,big bash leagend'
+          />
+          <title>Live Cricket Score</title>
+        </Helmet>
+
         <Routes>
           <Route path="/" exact element={<Current_Matches />} />
           <Route path="/matches/upcomming-matches" element={<Upcomming_Matches />} />
